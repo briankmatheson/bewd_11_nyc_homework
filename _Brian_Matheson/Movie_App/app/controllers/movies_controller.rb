ba8cc2704fff4b7ae93221ab919835c4503b6c7f
Brian Matheson <brian.k.matheson@gmail.com>
2014-07-23 17:20:25 -0400

    add search functionality

--- _Brian_Matheson/Movie_App/app/controllers/movies_controller.rb
+++ _Brian_Matheson/Movie_App/app/controllers/movies_controller.rb
@@ -1,6 +1,11 @@
 class MoviesController < ApplicationController
 
   def index
+    if params[:q]
+      @movie = Movie.search(params[:q])
+    else
+      @movie = Movie.all
+    end
   end
 
   def show
--- _Brian_Matheson/Movie_App/app/models/movie.rb
+++ _Brian_Matheson/Movie_App/app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+  def self.search(query)
+    Movie.where('title LIKE :query', query:"%#{query}%")
+  end
+
 end
