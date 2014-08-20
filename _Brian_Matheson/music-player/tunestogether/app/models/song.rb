522dc24668b15725e01f4bfa035bdfa2ef4c3ae7
Brian Matheson <brian.k.matheson@gmail.com>
2014-08-20 15:21:07 -0400

    implement song and datafile models

--- _Brian_Matheson/music-player/tunestogether/app/models/song.rb
+++ _Brian_Matheson/music-player/tunestogether/app/models/song.rb
@@ -1,11 +1,15 @@
+require "mp3info"
+
 class Song < ActiveRecord::Base
-  # def import_new_song(filename)
-  #   Mp3Info.open(filename) do |mp3|
-  #     params[:name] = mp3.tag.title   
-  #     params[:album] = mp3.tag.album
-  #     params[:artist] = mp3.tag.artist   
-  #     # mp3.tag.tracknum
-  #   end
-  #   Song.create(params)
-  # end
+  
+  def self.import_new_song(filename)
+    Mp3Info.open(filename) do |mp3|
+      params = {}
+      params[:name] = mp3.tag.title   
+      params[:album] = mp3.tag.album
+      params[:artist] = mp3.tag.artist   
+      # mp3.tag.tracknum
+      Song.create(params)
+    end
+  end
 end
--- _Brian_Matheson/music-player/tunestogether/spec/models/song_spec.rb
+++ _Brian_Matheson/music-player/tunestogether/spec/models/song_spec.rb
@@ -9,16 +9,22 @@ RSpec.describe Song, :type => :model do
       Song.create(params)
     end
 
-    # let!(:import) do
-    #   Song.import_new_song("public/01 Bubba Slide.mp3")
-    # end
+    let(:import) do
+      Song.import_new_song("spec/01 Bubba Slide.mp3")
+    end
 
     it "creates a new song" do
       expect(Song.count).to eq 1
     end
-    # it "imports a song" do
-    #   expect(import.id).to be_present
-    # end
+    it "imports a song" do
+      expect(import.id).to be_present
+    end
+    it "decodes the id tag" do
+      expect(import.name).to be_present
+    end
+    it "saves the correct name" do
+      expect(import.name).to eq "Bubba Slide"
+    end
   end
 end
 
